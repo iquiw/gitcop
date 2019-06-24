357b3777aef4732f1fb654a26fa46cf49c4c7aab
Iku Iwasa <iku.iwasa@gmail.com>
2019-06-25 07:17:41 +0900

    Change marks in list command
    - "*": Default repo & exist
    - "-": Default repo & not exist
    - "o": Optional repo & exist
    - " ": Optional repo & not exist

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -7,21 +7,12 @@ use crate::config::{Config, Remote, RepoKind};
 pub fn list(cfg: &Config) {
     for result in cfg.repos(None) {
         if let Ok((dir, repo_kind)) = result {
-            let repo = match repo_kind {
-                RepoKind::Default(repo) => repo,
-                RepoKind::Optional(repo) => repo,
+            let exist = Path::new(dir).is_dir();
+            let (mark, repo) = match repo_kind {
+                RepoKind::Default(repo) => (if exist { "*" } else { "-" }, repo),
+                RepoKind::Optional(repo) => (if exist { "o" } else { " " }, repo),
             };
-            let path = Path::new(dir);
-            println!(
-                "{} {:<19} {}",
-                if path.is_dir() {
-                    Green.paint("o")
-                } else {
-                    " ".into()
-                },
-                dir,
-                repo.url()
-            );
+            println!("{} {:<19} {}", Green.paint(mark), dir, repo.url());
         }
     }
 }
