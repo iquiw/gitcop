855372fb360296ccb618e324368b66c02c390c00
Iku Iwasa <iku.iwasa@gmail.com>
2021-07-11 23:32:57 +0900

    clippy police

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -7,43 +7,39 @@ use crate::config::{Config, Remote, Selection};
 use crate::print;
 
 pub fn list(cfg: &Config, default: bool, optional: bool) -> Result<(), Error> {
-    for result in cfg.repos(None) {
-        if let Ok((dir, select)) = result {
-            let exist = Path::new(dir).is_dir();
-            let (mark, repo) = match select {
-                Selection::Explicit(repo) => {
-                    if !default {
-                        continue;
-                    } else {
-                        (if exist { "*" } else { "-" }, repo)
-                    }
+    for (dir, select) in cfg.repos(None).flatten() {
+        let exist = Path::new(dir).is_dir();
+        let (mark, repo) = match select {
+            Selection::Explicit(repo) => {
+                if !default {
+                    continue;
+                } else {
+                    (if exist { "*" } else { "-" }, repo)
                 }
-                Selection::Optional(repo) => {
-                    if !optional {
-                        continue;
-                    } else {
-                        (if exist { "o" } else { " " }, repo)
-                    }
+            }
+            Selection::Optional(repo) => {
+                if !optional {
+                    continue;
+                } else {
+                    (if exist { "o" } else { " " }, repo)
                 }
-            };
-            println!("{} {:<19} {}", print::good(mark), dir, repo.url());
-        }
+            }
+        };
+        println!("{} {:<19} {}", print::good(mark), dir, repo.url());
     }
     Ok(())
 }
 
 pub fn list_unknown(cfg: &Config) -> Result<(), Error> {
     let rdir = fs::read_dir(".").map_err(|e| anyhow!("Unable to read directory, {}", e))?;
-    for result in rdir {
-        if let Ok(entry) = result {
-            if !entry.path().is_dir() {
-                continue;
-            }
-            let file_name = entry.file_name();
-            let name = file_name.to_string_lossy();
-            if !cfg.is_known(&name) && !name.starts_with('.') {
-                println!("{}", name);
-            }
+    for entry in rdir.flatten() {
+        if !entry.path().is_dir() {
+            continue;
+        }
+        let file_name = entry.file_name();
+        let name = file_name.to_string_lossy();
+        if !cfg.is_known(&name) && !name.starts_with('.') {
+            println!("{}", name);
         }
     }
     Ok(())
